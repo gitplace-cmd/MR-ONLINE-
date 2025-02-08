39be2a6dda0f38db30177ac3b1e65ded8525a5f3
Mronline23 <logosmrnetworker@gmail.com>
2025-02-08 23:53:53 +0300

    Update reboot.js

--- commandes/reboot.js
+++ commandes/reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("YESSER-MD  bot Restarting ⏳");
+    repondre("MR ONLINE-MD  bot Restarting ⏳");
 
   exec("pm2 restart all");
   
