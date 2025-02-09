d39134098cbadcd183f5f4287bcd0c56d6a02a17
Mronline23 <logosmrnetworker@gmail.com>
2025-02-09 08:26:45 +0300

    Update fremedia.js

--- commandes/fremedia.js
+++ commandes/fremedia.js
@@ -24,7 +24,7 @@ zokou({
       'image': {
         'url': _0x93b128
       },
-      'caption': "*powered by YESSER_MD*"
+      'caption': "*powered by MR ONLINE_MD*"
     }, {
       'quoted': _0x4eb9fe
     });
