5c71ba1789eeee0fd8a0711e58109796f738fc23
Mronline23 <logosmrnetworker@gmail.com>
2025-02-09 07:06:02 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "repo", catégorie:"Général", reaction: "😒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = https://github.com/Mronline23/MR-ONLINE-
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "😟", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://github.com/Mronline23/MR-ONLINE-'
   const img = 'https://files.catbox.moe/iaqbhv.jpg';
 
   try {
